b89997ec76e08a8dbc37c8915f76dcca4a635d09
makyz44 <matija.s.stankovic@gmail.com>
2018-06-28 22:07:45 +0200

    Add files via upload

---
+++ randomplayer.py
@@ -0,0 +1,25 @@
+import os
+import random
+import time
+import funkcija
+import kombinacije
+
+nazkolone=[kombinacije.nazkombd, kombinacije.nazkombg, kombinacije.nazkombsp1, kombinacije.nazkombsp2, kombinacije.nazkombun1, kombinacije.nazkombun2, kombinacije.nazkombs]
+reznazkolone=[kombinacije.reznazkombd, kombinacije.reznazkombg, kombinacije.reznazkombsp1, kombinacije.reznazkombsp2, kombinacije.reznazkombun1, kombinacije.reznazkombun2, kombinacije.reznazkombs]
+vredkolone=[kombinacije.vredkombd, kombinacije.vredkombg, kombinacije.vredkombsp1, kombinacije.vredkombsp2, kombinacije.vredkombun1, kombinacije.vredkombun2, kombinacije.vredkombs]
+rezvredkolone=[kombinacije.rezvredkombd, kombinacije.rezvredkombg, kombinacije.rezvredkombsp1, kombinacije.rezvredkombsp2, kombinacije.rezvredkombun1, kombinacije.rezvredkombun2, kombinacije.rezvredkombs]
+
+brojpoteza=65
+brojac=0
+while brojac<brojpoteza:
+    
+########STRATEGIJA##############
+
+print(vredkolone);
+
+
+
+
+
+
+
