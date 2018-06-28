a7ad3f79ca3ab7a26ecc37f36e8899454e6b3f8c
makyz44 <matija.s.stankovic@gmail.com>
2018-06-28 22:06:58 +0200

    Add files via upload

---
+++ kombinacije.py
@@ -0,0 +1,40 @@
+import os
+import random
+import funkcija
+
+
+reznazkombd = []
+reznazkombs = []
+reznazkombg = []
+reznazkombsp1 = []
+reznazkombsp2 = []
+reznazkombun1 = []
+reznazkombun2 = []
+
+rezvredkombd = []
+rezvredkombs = []
+rezvredkombg = []
+rezvredkombsp1 = []
+rezvredkombsp2 = []
+rezvredkombun1 = []
+rezvredkombun2 = []
+
+nazkombd = ["jedinice", "dvojke", "trojke", "cetvorke", "petice", "sestice", "maximum", "minimum", "kenta", "triling", "ful", "kare", "jamb"]
+vredkombd = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+nazkombs = ["jedinice", "dvojke", "trojke", "cetvorke", "petice", "sestice", "maximum", "minimum", "kenta", "triling", "ful", "kare", "jamb"]
+vredkombs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+nazkombg = ["jamb", "kare", "ful", "triling", "kenta", "minimum", "maximum", "sestice", "petice", "cetvorke", "trojke", "dvojke", "jedinice"]
+vredkombg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+nazkombsp1= ["maximum", "sestice", "petice", "cetvorke", "trojke", "dvojke", "jedinice"]
+vredkombsp1 = [0, 0, 0, 0, 0, 0, 0]
+nazkombsp2= ["minimum", "kenta", "triling", "ful", "kare", "jamb"]
+vredkombsp2 = [0, 0, 0, 0, 0, 0]
+
+nazkombun1= ["jedinice", "dvojke", "trojke", "cetvorke", "petice", "sestice", "maximum"]
+vredkombun1 = [0, 0, 0, 0, 0, 0, 0]
+nazkombun2= ["jamb", "kare", "ful", "triling", "kenta", "minimum"]
+vredkombun2 = [0, 0, 0, 0, 0, 0]
+
