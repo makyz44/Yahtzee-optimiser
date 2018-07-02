4c6946eed5658e9c917929df84fad46a08f87888
makyz44 <matija.s.stankovic@gmail.com>
2018-07-02 17:31:09 +0200

    Add files via upload

--- Rac2018.py
+++ Rac2018.py
@@ -5,10 +5,10 @@ import funkcije
 import kombinacije
 
 def heuristika1():
-    nazkolone=[kombinacije.nazkombd, kombinacije.nazkombg, kombinacije.nazkombsp1, kombinacije.nazkombsp2, kombinacije.nazkombun1, kombinacije.nazkombun2, kombinacije.nazkombs]
-    reznazkolone=[kombinacije.reznazkombd, kombinacije.reznazkombg, kombinacije.reznazkombsp1, kombinacije.reznazkombsp2, kombinacije.reznazkombun1, kombinacije.reznazkombun2, kombinacije.reznazkombs]
-    vredkolone=[kombinacije.vredkombd, kombinacije.vredkombg, kombinacije.vredkombsp1, kombinacije.vredkombsp2, kombinacije.vredkombun1, kombinacije.vredkombun2, kombinacije.vredkombs]
-    rezvredkolone=[kombinacije.rezvredkombd, kombinacije.rezvredkombg, kombinacije.rezvredkombsp1, kombinacije.rezvredkombsp2, kombinacije.rezvredkombun1, kombinacije.rezvredkombun2, kombinacije.rezvredkombs]
+    nazkolone=[kombinacije.nazkombd[:], kombinacije.nazkombg[:], kombinacije.nazkombsp1[:], kombinacije.nazkombsp2[:], kombinacije.nazkombun1[:], kombinacije.nazkombun2[:], kombinacije.nazkombs[:]]
+    reznazkolone=[kombinacije.reznazkombd[:], kombinacije.reznazkombg[:], kombinacije.reznazkombsp1[:], kombinacije.reznazkombsp2[:], kombinacije.reznazkombun1[:], kombinacije.reznazkombun2[:], kombinacije.reznazkombs[:]]
+    vredkolone=[kombinacije.vredkombd[:], kombinacije.vredkombg[:], kombinacije.vredkombsp1[:], kombinacije.vredkombsp2[:], kombinacije.vredkombun1[:], kombinacije.vredkombun2[:], kombinacije.vredkombs[:]]
+    rezvredkolone=[kombinacije.rezvredkombd[:], kombinacije.rezvredkombg[:], kombinacije.rezvredkombsp1[:], kombinacije.rezvredkombsp2[:], kombinacije.rezvredkombun1[:], kombinacije.rezvredkombun2[:], kombinacije.rezvredkombs[:]]
 
     brojpoteza=65
     brojac=0
@@ -82,11 +82,37 @@ def heuristika1():
     brojbodova34=funkcije.suma(rezvredkolone[2])+funkcije.suma(rezvredkolone[3])-(rezvredkolone[2][0]+rezvredkolone[3][0])+(rezvredkolone[2][0]-rezvredkolone[3][0])
     brojbodova56=funkcije.suma(rezvredkolone[4])+funkcije.suma(rezvredkolone[5])-(rezvredkolone[4][6]+rezvredkolone[5][5])+(rezvredkolone[4][6]-rezvredkolone[5][5])
     brojbodovaslobodne=funkcije.suma(rezvredkolone[6])-(rezvredkolone[6][6]+rezvredkolone[6][7])+(rezvredkolone[6][6]-rezvredkolone[6][7])
-    ukupnobodova=brojbodova0+brojbodova1+brojbodova34+brojbodova56+brojbodovaslobodne
+    bonus11=0
+    bonus22=0
+    bonus33=0
+    bonus44=0
+    bonus1=0
+    for i in range(6):
+        bonus1=bonus1+rezvredkolone[0][i]
+    if bonus1>=60:
+        bonus11=30
+    bonus2=0
+    for i in range(7, 13):
+        bonus2=bonus2+rezvredkolone[1][i]
+    if bonus2>=60:
+        bonus22=30
+    bonus3=0
+    for i in range(1, 7):
+        bonus3=bonus3+rezvredkolone[2][i]
+    if bonus3>=60:
+        bonus33=30
+    bonus4=0
+    for i in range(6):
+        bonus4=bonus4+rezvredkolone[5][i]
+    if bonus4>=60:
+        bonus44=30
+    ukupnobodova=brojbodova0+brojbodova1+brojbodova34+brojbodova56+brojbodovaslobodne+bonus11+bonus22+bonus33+bonus44
     return ukupnobodova
 
-print(heuristika1())
-
+b=0
+for i in range(100):
+    b=heuristika1()+b
+print(b/100)
 
 
     
