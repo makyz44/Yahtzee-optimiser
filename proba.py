c8783eb4b39f23fe76f829ec39afb9e212a5a749
makyz44 <matija.s.stankovic@gmail.com>
2018-07-02 17:30:51 +0200

    Add files via upload

---
+++ proba.py
@@ -0,0 +1,16 @@
+import os 
+import random
+import funkcije
+import math
+import kombinacije
+
+kombinacija=[1, 1, 2, 3, 3]
+bodovi=funkcije.bodovi(kombinacija, "jamb")
+najboljakombinacija=funkcije.najpogodnijiniz(kombinacije, "jamb")
+verovatnoca=funkcije.vrv(kombinacija, najboljakombinacija)
+koeficijenat=
+
+
+
+
+    
